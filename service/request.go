089375ca488bd3b36dccfd6be8e7462daa4d3bdb
wangminxiang <ryougi.nevermore@hotmail.com>
2023-03-28 16:54:00 +0800

    rebuild versions and header

--- service/request.go
+++ service/request.go
@@ -22,62 +22,245 @@ import (
 	"github.com/aacfactory/errors"
 	"github.com/aacfactory/fns/commons/uid"
 	"github.com/aacfactory/fns/commons/versions"
+	"github.com/aacfactory/fns/commons/wildcard"
 	"github.com/aacfactory/json"
 	"net/http"
 	"strconv"
 	"strings"
 )
 
-type RequestHeader interface {
-	Contains(key string) (ok bool)
-	Get(key string) (value string)
-	Values(key string) (values []string)
-	Set(key string, value string)
-	Add(key string, value string)
-	ForEach(fn func(key string, values []string) (next bool))
-	DeviceId() (id string)
-	DeviceIp() (ip string)
-	Authorization() (authorization string, has bool)
-	VersionRange() (left versions.Version, right versions.Version, err error)
-	MapToHttpHeader() (v http.Header)
+var (
+	allowAllRequestVersions = RequestVersions{
+		{
+			Pattern: "*",
+			Begin:   versions.Min(),
+			End:     versions.Max(),
+			Exact:   false,
+		},
+	}
+)
+
+func AllowAllRequestVersions() RequestVersions {
+	return allowAllRequestVersions
 }
 
-func newRequestHeader() RequestHeader {
-	return &requestHeader{value: make(map[string][]string)}
-}
+type RequestVersions []*RequestVersion
 
-type requestHeader struct {
-	value http.Header
-}
-
-func (header *requestHeader) Contains(key string) (ok bool) {
-	_, ok = header.value[key]
+func (rvs RequestVersions) Accept(serviceName string, target versions.Version) (ok bool) {
+	if rvs == nil || len(rvs) == 0 {
+		return
+	}
+	for _, rv := range rvs {
+		if rv == nil {
+			continue
+		}
+		if rv.Accept(serviceName, target) {
+			return true
+		}
+	}
 	return
 }
 
-func (header *requestHeader) Get(key string) (value string) {
-	value = header.value.Get(key)
+func (rvs RequestVersions) Empty() (ok bool) {
+	ok = rvs == nil || len(rvs) == 0
 	return
 }
 
-func (header *requestHeader) Values(key string) (values []string) {
-	values = header.value.Values(key)
+func (rvs RequestVersions) String() string {
+	if rvs == nil {
+		return "nil"
+	}
+	ss := make([]string, 0, 1)
+	for _, rv := range rvs {
+		if rv == nil {
+			continue
+		}
+		ss = append(ss, rv.String())
+	}
+	return fmt.Sprintf("[%s]", strings.Join(ss, ", "))
+}
+
+func ParseRequestVersionFromHeader(header http.Header) (rvs RequestVersions, has bool, err error) {
+	values := header.Values(httpRequestVersionsHeader)
+	if values == nil || len(values) == 0 {
+		return
+	}
+	rvs = make([]*RequestVersion, 0, 1)
+	for _, value := range values {
+		if value == "" {
+			continue
+		}
+		rv, parseErr := ParseRequestVersion(value)
+		if parseErr != nil {
+			err = parseErr
+			return
+		}
+		rvs = append(rvs, rv)
+	}
+	has = len(rvs) > 0
 	return
 }
 
-func (header *requestHeader) Set(key string, value string) {
-	header.value.Set(key, value)
+func ParseRequestVersion(s string) (rv *RequestVersion, err error) {
+	if s == "" {
+		err = errors.Warning("fns: parse request version failed").WithCause(errors.Warning("value is nil"))
+		return
+	}
+	idx := strings.Index(s, "=")
+	if idx < 1 {
+		err = errors.Warning("fns: parse request version failed").WithCause(errors.Warning("no pattern"))
+		return
+	}
+	pattern := strings.TrimSpace(s[0:idx])
+	s = s[idx+1:]
+	idx = strings.Index(s, ":")
+	if idx < 0 {
+		beg, parseBegErr := versions.Parse(s)
+		if parseBegErr != nil {
+			err = errors.Warning("fns: parse request version failed").WithCause(parseBegErr)
+			return
+		}
+		rv = &RequestVersion{
+			Pattern: pattern,
+			Begin:   beg,
+			End:     versions.Version{},
+			Exact:   true,
+		}
+		return
+	}
+	beg := versions.Version{}
+	begValue := strings.TrimSpace(s[0:idx])
+	if begValue != "" {
+		beg, err = versions.Parse(begValue)
+		if err != nil {
+			err = errors.Warning("fns: parse request version failed").WithCause(err)
+			return
+		}
+	}
+	end := versions.Version{}
+	endValue := strings.TrimSpace(s[idx+1:])
+	if endValue != "" {
+		end, err = versions.Parse(endValue)
+		if err != nil {
+			err = errors.Warning("fns: parse request version failed").WithCause(err)
+			return
+		}
+	}
+	rv = &RequestVersion{
+		Pattern: pattern,
+		Begin:   beg,
+		End:     end,
+		Exact:   false,
+	}
+	return
 }
 
-func (header *requestHeader) Add(key string, value string) {
-	header.value.Add(key, value)
+type RequestVersion struct {
+	Pattern string
+	Begin   versions.Version
+	End     versions.Version
+	Exact   bool
 }
 
-func (header *requestHeader) ForEach(fn func(key string, values []string) (next bool)) {
+func (rv *RequestVersion) Accept(serviceName string, target versions.Version) (ok bool) {
+	if !wildcard.Match(rv.Pattern, serviceName) {
+		return
+	}
+	if rv.Exact {
+		ok = rv.Begin.Equals(target)
+		return
+	}
+	ok = target.Between(rv.Begin, rv.End)
+	return
+}
+
+func (rv *RequestVersion) String() string {
+	if rv.Exact {
+		return fmt.Sprintf("%s=%s", rv.Pattern, rv.Begin.String())
+	}
+	return fmt.Sprintf("%s=%s:%s", rv.Pattern, rv.Begin.String(), rv.End.String())
+}
+
+type RequestHeader http.Header
+
+func (header RequestHeader) Empty() bool {
+	return header == nil || len(header) == 0
+}
+
+func (header RequestHeader) Add(key, value string) {
+	http.Header(header).Add(key, value)
+}
+
+func (header RequestHeader) Set(key, value string) {
+	http.Header(header).Set(key, value)
+}
+
+func (header RequestHeader) Get(key string) string {
+	return http.Header(header).Get(key)
+}
+
+func (header RequestHeader) Values(key string) []string {
+	return http.Header(header).Values(key)
+}
+
+func (header RequestHeader) Del(key string) {
+	http.Header(header).Del(key)
+}
+
+func (header RequestHeader) Clone() RequestHeader {
+	return RequestHeader(http.Header(header).Clone())
+}
+
+func (header RequestHeader) Contains(key string) (ok bool) {
+	ok = http.Header(header).Get(key) != ""
+	return
+}
+
+func (header RequestHeader) Authorization() (authorization string, has bool) {
+	authorization = http.Header(header).Get("Authorization")
+	has = authorization != ""
+	return
+}
+
+func (header RequestHeader) SetDeviceId(id string) {
+	http.Header(header).Set(httpDeviceIdHeader, id)
+	return
+}
+
+func (header RequestHeader) DeviceId() (id string) {
+	id = http.Header(header).Get(httpDeviceIdHeader)
+	return
+}
+
+func (header RequestHeader) SetDeviceIp(ip string) {
+	http.Header(header).Set(httpDeviceIpHeader, ip)
+	return
+}
+
+func (header RequestHeader) DeviceIp() (id string) {
+	id = http.Header(header).Get(httpDeviceIpHeader)
+	return
+}
+
+func (header RequestHeader) SetAcceptVersions(rvs RequestVersions) {
+	header.Del(httpRequestVersionsHeader)
+	if rvs == nil || len(rvs) == 0 {
+		return
+	}
+	for _, rv := range rvs {
+		if rv == nil {
+			return
+		}
+		header.Add(httpRequestVersionsHeader, rv.String())
+	}
+	return
+}
+
+func (header RequestHeader) ForEach(fn func(key string, values []string) (next bool)) {
 	if fn == nil {
 		return
 	}
-	for key, values := range header.value {
+	for key, values := range header {
 		next := fn(key, values)
 		if !next {
 			break
@@ -85,55 +268,63 @@ func (header *requestHeader) ForEach(fn func(key string, values []string) (next 
 	}
 }
 
-func (header *requestHeader) Authorization() (authorization string, has bool) {
-	authorization = header.Get("Authorization")
-	has = authorization != ""
-	return
+type ResponseHeader http.Header
+
+func (header ResponseHeader) Empty() bool {
+	return header == nil || len(header) == 0
 }
 
-func (header *requestHeader) DeviceId() (id string) {
-	id = header.Get(httpDeviceIdHeader)
-	return
+func (header ResponseHeader) Add(key, value string) {
+	http.Header(header).Add(key, value)
 }
 
-func (header *requestHeader) DeviceIp() (id string) {
-	id = header.Get(httpDeviceIpHeader)
-	return
+func (header ResponseHeader) Set(key, value string) {
+	http.Header(header).Set(key, value)
 }
 
-func (header *requestHeader) VersionRange() (left versions.Version, right versions.Version, err error) {
-	version := header.Get(httpRequestVersionsHeader)
-	if version == "" {
-		right = versions.Max()
+func (header ResponseHeader) Get(key string) string {
+	return http.Header(header).Get(key)
+}
+
+func (header ResponseHeader) Values(key string) []string {
+	return http.Header(header).Values(key)
+}
+
+func (header ResponseHeader) Del(key string) {
+	http.Header(header).Del(key)
+}
+
+func (header ResponseHeader) EnableCacheControl(age int64) {
+	if age < 1 {
 		return
 	}
-	versionRange := strings.Split(version, ",")
-	leftVersionValue := strings.TrimSpace(versionRange[0])
-	if leftVersionValue != "" {
-		left, err = versions.Parse(leftVersionValue)
-		if err != nil {
-			err = errors.Warning("fns: read request version failed").WithCause(err)
-			return
-		}
-	}
-	if len(versionRange) > 1 {
-		rightVersionValue := strings.TrimSpace(versionRange[1])
-		if rightVersionValue != "" {
-			right, err = versions.Parse(rightVersionValue)
-			if err != nil {
-				err = errors.Warning("fns: read request version failed").WithCause(err)
-				return
-			}
-		}
-	} else {
-		right = versions.Max()
-	}
+	http.Header(header).Set(httpCacheControlHeader, fmt.Sprintf("max-age=%d", age))
 	return
 }
 
-func (header *requestHeader) MapToHttpHeader() (v http.Header) {
-	v = header.value
+func (header ResponseHeader) DisableCacheControl() {
+	http.Header(header).Set(httpCacheControlHeader, "no-cache, private")
+}
+
+func (header ResponseHeader) Clone() ResponseHeader {
+	return ResponseHeader(http.Header(header).Clone())
+}
+
+func (header ResponseHeader) Contains(key string) (ok bool) {
+	ok = http.Header(header).Get(key) != ""
 	return
+}
+
+func (header ResponseHeader) ForEach(fn func(key string, values []string) (next bool)) {
+	if fn == nil {
+		return
+	}
+	for key, values := range header {
+		next := fn(key, values)
+		if !next {
+			break
+		}
+	}
 }
 
 // +-------------------------------------------------------------------------------------------------------------------+
@@ -316,12 +507,14 @@ func (trunk *requestTrunk) Remove(key string) {
 type Request interface {
 	Id() (id string)
 	Header() (header RequestHeader)
-	ResponseHeader() (header http.Header)
+	ResponseHeader() (header ResponseHeader)
+	AcceptedVersions() (acceptedVersions RequestVersions)
 	Fn() (service string, fn string)
 	Argument() (argument Argument)
 	Internal() (ok bool)
 	User() (user RequestUser)
 	Trunk() (trunk RequestTrunk)
+	CacheControlEnabled() (ok bool)
 }
 
 type RequestOption func(*RequestOptions)
@@ -334,17 +527,13 @@ func WithRequestId(id string) RequestOption {
 
 func WithHttpRequestHeader(header http.Header) RequestOption {
 	return func(options *RequestOptions) {
-		for key, values := range header {
-			for _, value := range values {
-				options.header.Add(key, value)
-			}
-		}
+		options.header = RequestHeader(header)
 	}
 }
 
 func WithHttpResponseHeader(header http.Header) RequestOption {
 	return func(options *RequestOptions) {
-		options.responseHeader = header
+		options.responseHeader = ResponseHeader(header)
 	}
 }
 
@@ -378,119 +567,158 @@ func WithRequestTrunk(trunk RequestTrunk) RequestOption {
 	}
 }
 
-type RequestOptions struct {
-	id             string
-	header         RequestHeader
-	responseHeader http.Header
-	deviceId       string
-	deviceIp       string
-	internal       bool
-	user           RequestUser
-	trunk          RequestTrunk
+func WithRequestVersions(acceptedVersions RequestVersions) RequestOption {
+	return func(options *RequestOptions) {
+		options.acceptedVersions = acceptedVersions
+	}
 }
 
-func NewRequest(ctx context.Context, service string, fn string, argument Argument, options ...RequestOption) (v Request) {
+func EnableCacheControl() RequestOption {
+	return func(options *RequestOptions) {
+		options.cacheControlEnabled = true
+	}
+}
+
+type RequestOptions struct {
+	id                  string
+	header              RequestHeader
+	responseHeader      ResponseHeader
+	acceptedVersions    RequestVersions
+	deviceId            string
+	deviceIp            string
+	internal            bool
+	user                RequestUser
+	trunk               RequestTrunk
+	cacheControlEnabled bool
+}
+
+func NewRequest(ctx context.Context, service string, fn string, arg Argument, options ...RequestOption) (v Request) {
 	opt := &RequestOptions{
-		id:             "",
-		header:         newRequestHeader(),
-		responseHeader: http.Header{},
-		deviceId:       "",
-		deviceIp:       "",
-		user:           nil,
-		internal:       false,
-		trunk:          newRequestTrunk(),
+		id:                  "",
+		header:              RequestHeader{},
+		responseHeader:      ResponseHeader{},
+		acceptedVersions:    nil,
+		deviceId:            "",
+		deviceIp:            "",
+		user:                nil,
+		internal:            false,
+		trunk:               nil,
+		cacheControlEnabled: false,
 	}
 	if options != nil && len(options) > 0 {
 		for _, option := range options {
 			option(opt)
 		}
 	}
-	if opt.deviceIp != "" {
-		opt.header.Add(httpDeviceIpHeader, opt.deviceIp)
+	if arg == nil {
+		arg = EmptyArgument()
 	}
-
 	prev, hasPrev := GetRequest(ctx)
 	if hasPrev {
-		id := opt.id
-		internal := false
-		if id == "" {
-			id = prev.Id()
-			internal = true
-		} else {
-			internal = opt.internal
+		id := prev.Id()
+		if opt.id != "" {
+			id = opt.id
 		}
 		header := prev.Header()
-		if len(opt.header.MapToHttpHeader()) > 0 {
-			opt.header.ForEach(func(key string, values []string) (next bool) {
-				for _, value := range values {
-					header.Add(key, value)
-				}
-				next = true
-				return
-			})
+		if !opt.header.Empty() {
+			header = opt.header
 		}
-		user := opt.user
-		if user == nil {
-			user = prev.User()
+		if opt.deviceId != "" {
+			header.SetDeviceId(opt.deviceId)
+		}
+		if opt.deviceIp != "" {
+			header.SetDeviceIp(opt.deviceIp)
+		}
+		user := prev.User()
+		if opt.user != nil {
+			user = opt.user
+		}
+		trunk := prev.Trunk()
+		if opt.trunk != nil {
+			trunk = opt.trunk
+		}
+		acceptedVersions := prev.AcceptedVersions()
+		if opt.acceptedVersions != nil {
+			acceptedVersions = opt.acceptedVersions
+		}
+		if acceptedVersions == nil {
+			acceptedVersions = AllowAllRequestVersions()
 		}
 		v = &request{
-			id:             id,
-			internal:       internal,
-			user:           user,
-			trunk:          prev.Trunk(),
-			header:         header,
-			responseHeader: prev.ResponseHeader(),
-			service:        service,
-			fn:             fn,
-			argument:       argument,
+			id:                  id,
+			internal:            true,
+			user:                user,
+			trunk:               trunk,
+			header:              header,
+			responseHeader:      prev.ResponseHeader(),
+			service:             service,
+			fn:                  fn,
+			argument:            arg,
+			acceptedVersions:    acceptedVersions,
+			cacheControlEnabled: opt.cacheControlEnabled,
 		}
 	} else {
 		id := opt.id
 		if id == "" {
 			id = uid.UID()
 		}
-		header := newRequestHeader()
-		if len(opt.header.MapToHttpHeader()) > 0 {
-			opt.header.ForEach(func(key string, values []string) (next bool) {
-				for _, value := range values {
-					header.Add(key, value)
-				}
-				next = true
-				return
-			})
+		var header RequestHeader
+		if opt.header != nil {
+			header = opt.header
+		} else {
+			header = RequestHeader{}
 		}
 		if opt.deviceId != "" {
-			header.Add(httpDeviceIdHeader, opt.deviceId)
+			header.SetDeviceId(opt.deviceId)
+		}
+		if opt.deviceIp != "" {
+			header.SetDeviceIp(opt.deviceIp)
 		}
 		user := opt.user
 		if user == nil {
 			user = NewRequestUser("", nil)
 		}
+		var trunk RequestTrunk
+		if opt.trunk != nil {
+			trunk = opt.trunk
+		} else {
+			trunk = newRequestTrunk()
+		}
+		var acceptedVersions RequestVersions
+		if opt.acceptedVersions != nil {
+			acceptedVersions = opt.acceptedVersions
+		} else {
+			acceptedVersions = AllowAllRequestVersions()
+		}
 		v = &request{
-			id:             id,
-			internal:       opt.internal,
-			user:           user,
-			trunk:          opt.trunk,
-			header:         header,
-			responseHeader: opt.responseHeader,
-			service:        service,
-			fn:             fn,
-			argument:       argument,
+			id:                  id,
+			internal:            opt.internal,
+			user:                user,
+			trunk:               trunk,
+			header:              header,
+			responseHeader:      opt.responseHeader,
+			service:             service,
+			fn:                  fn,
+			argument:            arg,
+			acceptedVersions:    acceptedVersions,
+			cacheControlEnabled: opt.cacheControlEnabled,
 		}
 	}
 	return
 }
 
 type request struct {
-	id             string
-	internal       bool
-	user           RequestUser
-	trunk          RequestTrunk
-	header         RequestHeader
-	responseHeader http.Header
-	service        string
-	fn             string
-	argument       Argument
+	id                  string
+	internal            bool
+	user                RequestUser
+	trunk               RequestTrunk
+	header              RequestHeader
+	responseHeader      ResponseHeader
+	acceptedVersions    RequestVersions
+	cacheControlEnabled bool
+	service             string
+	fn                  string
+	argument            Argument
 }
 
 func (r *request) Id() (id string) {
@@ -518,8 +746,18 @@ func (r *request) Header() (header RequestHeader) {
 	return
 }
 
-func (r *request) ResponseHeader() (header http.Header) {
+func (r *request) ResponseHeader() (header ResponseHeader) {
 	header = r.responseHeader
+	return
+}
+
+func (r *request) AcceptedVersions() (acceptedVersions RequestVersions) {
+	acceptedVersions = r.acceptedVersions
+	return
+}
+
+func (r *request) CacheControlEnabled() (ok bool) {
+	ok = r.cacheControlEnabled
 	return
 }
 
@@ -563,29 +801,27 @@ func GetRequestUser(ctx context.Context) (user RequestUser, authenticated bool) 
 }
 
 type internalRequestImpl struct {
-	User  *requestUser    `json:"user"`
-	Trunk *requestTrunk   `json:"trunk"`
-	Body  json.RawMessage `json:"body"`
+	User     *requestUser  `json:"user"`
+	Trunk    *requestTrunk `json:"trunk"`
+	Argument *argument     `json:"argument"`
 }
 
 type internalRequest struct {
-	User  RequestUser     `json:"user"`
-	Trunk RequestTrunk    `json:"trunk"`
-	Body  json.RawMessage `json:"body"`
+	User     RequestUser  `json:"user"`
+	Trunk    RequestTrunk `json:"trunk"`
+	Argument Argument     `json:"argument,omitempty"`
 }
 
 type internalResponseImpl struct {
-	User   *requestUser    `json:"user"`
-	Trunk  *requestTrunk   `json:"trunk"`
-	Span   *Span           `json:"Span"`
-	Header http.Header     `json:"header"`
-	Body   json.RawMessage `json:"body"`
+	User  *requestUser    `json:"user"`
+	Trunk *requestTrunk   `json:"trunk"`
+	Span  *Span           `json:"Span"`
+	Body  json.RawMessage `json:"body"`
 }
 
 type internalResponse struct {
-	User   RequestUser     `json:"user"`
-	Trunk  RequestTrunk    `json:"trunk"`
-	Span   *Span           `json:"Span"`
-	Header http.Header     `json:"header"`
-	Body   json.RawMessage `json:"body"`
+	User  RequestUser  `json:"user"`
+	Trunk RequestTrunk `json:"trunk"`
+	Span  *Span        `json:"Span"`
+	Body  interface{}  `json:"body,omitempty"`
 }

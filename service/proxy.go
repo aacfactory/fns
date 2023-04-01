ea253f1ff609831a38afb9d3b350454d940c292a
wangminxiang <ryougi.nevermore@hotmail.com>
2023-04-01 22:01:30 +0800

    mark todo

--- service/proxy.go
+++ service/proxy.go
@@ -262,6 +262,13 @@ func (handler *proxyHandler) handleDocuments(w http.ResponseWriter, r *http.Requ
 
 func (handler *proxyHandler) handleProxy(w http.ResponseWriter, r *http.Request) {
 
+	// TODO
+	/*
+		handler 本身是transport handler，在需要代理时，参考 httputil.ReverseProxy，
+		proxy的配置里增加websocket后端地址，如果有，则自己外面的handler里就不要websocket，
+		配置内容为upgrade的类型。
+		配置里再增加malloc，即read buffer size，必须是K的整数
+	*/
 	ctx := r.Context()
 	// wrap registrations to ReverseProxy
 	ctx = context.WithValue(ctx, proxyContextKey, 1)
@@ -271,7 +278,7 @@ func (handler *proxyHandler) handleProxy(w http.ResponseWriter, r *http.Request)
 		Transport:      nil, // dialer return Transport (fasthttp，自己实现，就map request to requestCTX，然后client调用)
 		FlushInterval:  0,
 		ErrorLog:       nil,
-		BufferPool:     nil,
+		BufferPool:     nil, // todo try use caches.malloc_heap and mmap 尝试，可以用，因为它为空时，创建了buf = make([]byte, 32*1024)，所以新malloc的参数是多少K，最小4k。
 		ModifyResponse: nil,
 		ErrorHandler:   nil,
 	}

ff26a30451e969c6515d7f813fda44e3b6fc1a47
wangminxiang <ryougi.nevermore@hotmail.com>
2023-11-16 04:23:26 +0800

    mod

--- log/context.go
+++ log/context.go
@@ -1,24 +1,22 @@
 package log
 
 import (
-	sc "context"
 	"fmt"
 	"github.com/aacfactory/errors"
-	"github.com/aacfactory/fns/commons/bytex"
 	"github.com/aacfactory/fns/context"
 	"github.com/aacfactory/logs"
 )
 
-const (
-	contextKey = "@fns:context:log"
+var (
+	contextKey = []byte("@fns:context:log")
 )
 
-func With(ctx sc.Context, v logs.Logger) sc.Context {
-	return context.WithValue(ctx, bytex.FromString(contextKey), v)
+func With(ctx context.Context, v logs.Logger) {
+	ctx.SetLocalValue(contextKey, v)
 }
 
-func Load(ctx sc.Context) logs.Logger {
-	v := ctx.Value(contextKey)
+func Load(ctx context.Context) logs.Logger {
+	v := ctx.LocalValue(contextKey)
 	if v == nil {
 		panic(fmt.Sprintf("%+v", errors.Warning("fns: there is no log in context")))
 		return nil

089ffbed8bfa7150827b5077025aabd9d83cec9c
wangminxiang <ryougi.nevermore@hotmail.com>
2023-11-21 02:06:34 +0800

    mod

---
+++ services/commons/fn.go
@@ -0,0 +1,168 @@
+package commons
+
+import (
+	"github.com/aacfactory/errors"
+	"github.com/aacfactory/fns/runtime"
+	"github.com/aacfactory/fns/services"
+	"github.com/aacfactory/fns/services/authorizations"
+	"github.com/aacfactory/fns/services/metrics"
+	"github.com/aacfactory/fns/services/permissions"
+)
+
+func NewFn(name string, readonly bool, internal bool, authorized bool, permission bool, metric bool, barrier bool, handler FnHandler, middlewares ...FnHandlerMiddleware) services.Fn {
+	if len(middlewares) > 0 {
+		handler = FnHandlerMiddlewares(middlewares).Handler(handler)
+	}
+	return &Fn{
+		name:       name,
+		internal:   internal,
+		readonly:   readonly,
+		authorized: authorized,
+		permission: permission,
+		metric:     metric,
+		barrier:    barrier,
+		handler:    handler,
+	}
+}
+
+type FnHandler func(r services.Request) (v interface{}, err error)
+
+type FnHandlerMiddleware interface {
+	Handler(next FnHandler) FnHandler
+}
+
+type FnHandlerMiddlewares []FnHandlerMiddleware
+
+func (middlewares FnHandlerMiddlewares) Handler(handler FnHandler) FnHandler {
+	if len(middlewares) == 0 {
+		return handler
+	}
+	for i := len(middlewares) - 1; i > -1; i-- {
+		handler = middlewares[i].Handler(handler)
+	}
+	return handler
+}
+
+type Fn struct {
+	name       string
+	internal   bool
+	readonly   bool
+	authorized bool
+	permission bool
+	metric     bool
+	barrier    bool
+	handler    FnHandler
+}
+
+func (fn *Fn) Name() string {
+	return fn.name
+}
+
+func (fn *Fn) Internal() bool {
+	return fn.internal
+}
+
+func (fn *Fn) Readonly() bool {
+	return fn.readonly
+}
+
+func (fn *Fn) Handle(r services.Request) (v interface{}, err error) {
+	if fn.internal && !r.Header().Internal() {
+		err = errors.NotAcceptable("fns: fn cannot be accessed externally")
+		return
+	}
+	if fn.barrier {
+		var key []byte
+		if fn.authorized {
+			key, err = services.HashRequest(r, services.HashRequestWithToken())
+		} else {
+			key, err = services.HashRequest(r)
+		}
+		if err != nil {
+			return
+		}
+		if fn.metric {
+			metrics.Begin(r)
+		}
+		v, err = runtime.Barrier(r, key, func() (result interface{}, err error) {
+			// authorization
+			if fn.authorized {
+				err = fn.verifyAuthorization(r)
+				if err != nil {
+					return
+				}
+			}
+			// permission
+			if fn.permission {
+				err = fn.verifyPermission(r)
+				if err != nil {
+					return
+				}
+			}
+			// handle
+			result, err = fn.handler(r)
+			return
+		})
+		if fn.metric {
+			metrics.EndWithCause(r, err)
+		}
+	} else {
+		if fn.metric {
+			metrics.Begin(r)
+		}
+		// authorization
+		if fn.authorized {
+			err = fn.verifyAuthorization(r)
+			if err != nil {
+				if fn.metric {
+					metrics.EndWithCause(r, err)
+				}
+				return
+			}
+		}
+		// permission
+		if fn.permission {
+			err = fn.verifyPermission(r)
+			if err != nil {
+				if fn.metric {
+					metrics.EndWithCause(r, err)
+				}
+				return
+			}
+		}
+		// handle
+		v, err = fn.handler(r)
+		if fn.metric {
+			metrics.EndWithCause(r, err)
+		}
+	}
+	return
+}
+
+func (fn *Fn) verifyAuthorization(r services.Request) (err error) {
+	authorization := authorizations.Load(r)
+	if authorization.Exist() {
+		if !authorization.Validate() {
+			err = authorizations.ErrUnauthorized
+			return
+		}
+	} else {
+		token := r.Header().Token()
+		if len(token) == 0 {
+			err = authorizations.ErrUnauthorized
+			return
+		}
+		authorization, err = authorizations.Decode(r, token)
+		if err != nil {
+			err = authorizations.ErrUnauthorized.WithCause(err)
+			return
+		}
+		authorizations.With(r, authorization)
+	}
+	return
+}
+
+func (fn *Fn) verifyPermission(r services.Request) (err error) {
+	err = permissions.EnforceContext(r)
+	return
+}

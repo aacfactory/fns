5e82e557e4a726287b650d1cc8950cece49d1375
wangminxiang <ryougi.nevermore@hotmail.com>
2023-11-22 10:20:01 +0800

    update copyright

--- commons/caches/malloc_mmap.go
+++ commons/caches/malloc_mmap.go
@@ -1,7 +1,7 @@
 //go:build !appengine && !windows
 
 /*
- * Copyright 2021 Wang Min Xiang
+ * Copyright 2023 Wang Min Xiang
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,6 +14,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ *
  */
 
 package caches
